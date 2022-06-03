bb556a629925971c4f083b4ce6659056f88f2170
bielzzin <106832343+bielzzin@users.noreply.github.com>
2022-06-03 17:22:21 -0300

    Simulação Tela

--- src/Simulacao.js
+++ src/Simulacao.js
@@ -11,17 +11,64 @@ function Simulacao() {
 
   return (
     <div>
-         <Menu/> 
+        <Menu/> 
+        
+      <h1>    SIMULE SEU EMPRÉSTIMO PESSOAL </h1>
+                
+           
+      
+        <div className="box">
 
-        <h1>JYV</h1>
-        <button>Cadastro</button>
-        <button>Login</button>
-        <br/>
-        <hr/>
-        <h1>SIMULE SEU EMPRÉSTIMO
-            PESSOAL
-        </h1>
-    </div>
+            <div >
+                <input placeholder="Informe  seu CPF"></input>
+                <br/>
+                <br/>
+                <input placeholder="Informe seu e-mail"></input>
+                <br/>
+                <br/>
+                <input placeholder="Informe o valor desejado para empréstimo"></input>
+                <br/>
+                <br/>
+            </div>
+
+            <div >
+        
+                <label for="Banco"></label>
+
+                <select name="Banco" id=" Banco ">
+                    <option value="Brasil">Brasil</option>
+                    <option value="Bradesco">Bradesco</option>
+                    <option value="Santander">Santander</option>
+                    <option value="Itaú">Itaú</option>
+
+                </select>
+
+                <br/>
+                <br/>
+
+                <label for="Parcelamento"></label>
+
+                <select name="Parcelamento" id=" Parcelamento ">
+                    <option value="5X Sem juros">5X Sem juros</option>
+                    <option value="10X Sem juros">10X Sem juros</option>
+                    <option value="15X Sem juros">15X Sem juros</option>
+                    <option value="20X Sem juros">20X Sem juros</option>
+
+                </select>
+
+
+            
+            </div>
+
+            <br/>
+            <br/>
+            <button className="button1"> Solicitar Análise </button>
+
+        </div>
+   </div>
+
+
+
   )
 }
 
