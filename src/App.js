9f03b30e72367a1bd053392ca7ca496d25250b75
bielzzin <106832343+bielzzin@users.noreply.github.com>
2022-06-03 16:28:58 -0300

    página de simulcao

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Cadastro from './Cadastro';
 import Login from './Login';
 import Inicio from './Inicio';
+import Simulacao from './Simulacao';
 import { Routes, Route } from "react-router-dom";
 
 function App () {
@@ -15,6 +16,7 @@ function App () {
                 <Route path="/" element={<Inicio />} />
                 <Route path="/cadastro" element={<Cadastro />} />
                 <Route path="/login" element={<Login />} />
+                <Route path="/simulacao"element={<Simulacao/>}/>
             </Routes>
         </div>
     );
---
+++ src/Simulacao.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Menu from "./Menu";
+
+
+function Simulacao() {
+
+
+
+    require("./Simulacao.css");
+
+
+  return (
+    <div>
+         <Menu/> 
+
+        <h1>JYV</h1>
+        <button>Cadastro</button>
+        <button>Login</button>
+        <br/>
+        <hr/>
+        <h1>SIMULE SEU EMPRÉSTIMO
+            PESSOAL
+        </h1>
+    </div>
+  )
+}
+
+export default Simulacao
