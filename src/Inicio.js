22a3c78c7e96cc6419631249f9429d42c6f12d8b
Júlia Jóia <105455881+julia-joiab@users.noreply.github.com>
2022-06-03 17:10:52 -0300

    alterações

--- src/Inicio.js
+++ src/Inicio.js
@@ -3,15 +3,20 @@ import Menu from './Menu';
 import QuemSomos from './QuemSomos';
 import PerguntasFrequentes from './PerguntasFrequentes.js';
 import Avaliacoes from './Avaliacoes';
+import Rodape from './Rodape';
 
 const Inicio = () => {
+
     return ( 
 
         <div>
+
             <Menu/>
             <QuemSomos/>
             <PerguntasFrequentes/>
             <Avaliacoes/>
+            <Rodape/>
+
         </div>
 
      );
---
+++ src/Rodape.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Rodape = () => {
+
+    require('./Rodape.css')
+
+    return (  
+    
+        <div className='rodape'>
+
+            <div>
+
+                <h1> Fale conosco: </h1>
+
+                <p> </p>
+
+            </div>
+
+
+        </div>
+
+    );
+}
+ 
+export default Rodape;
