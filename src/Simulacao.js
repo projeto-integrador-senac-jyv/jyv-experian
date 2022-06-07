77f48994780c89caee824507b185424b794ba60f
bielzzin <106832343+bielzzin@users.noreply.github.com>
2022-06-07 16:48:51 -0300

    simulaca

--- src/Simulacao.js
+++ src/Simulacao.js
@@ -11,66 +11,74 @@ function Simulacao() {
 
   return (
     <div className="fundo">
+
         <Menu/> 
+
+        <div className="fundo3" >
         
-      
+
+            <div className='titulo' >
+
+                <h2> SIMULE SEU EMPRÉSTIMO PESSOAL </h2>
+                <img src='https://i.imgur.com/lOf8lLY.png' ></img>
                 
-           
-      
-        <div className="box3">
-            <h2>    SIMULE SEU EMPRÉSTIMO PESSOAL </h2>
-            <div >
-                <input placeholder="Informe  seu CPF"></input>
-                <br/>
-                <br/>
-                <input placeholder="Informe seu e-mail"></input>
-                <br/>
-                <br/>
-                <input placeholder="Informe o valor desejado para empréstimo"></input>
-                <br/>
-                <br/>
             </div>
-
-                
-
-
-            <div >
         
-                <label for="Banco"></label>
+            <div className="box3">
 
-                <select name="Banco" id=" Banco ">
+                <div >
+                    <input placeholder="Informe  seu CPF"></input>
+                    <br/>
+                    <br/>
+                    <input placeholder="Informe seu e-mail"></input>
+                    <br/>
+                    <br/>
+                    <input placeholder="Informe o valor desejado para empréstimo"></input>
+                    <br/>
+                    <br/>
+                </div>
 
-                    <option value="Escolha um Banco" >Escolha um Banco</option>
-                    <option value="Brasil">Brasil</option>
-                    <option value="Bradesco">Bradesco</option>
-                    <option value="Santander">Santander</option>
-                    <option value="Itaú">Itaú</option>
-
-                </select>
-
-                <br/>
-                <br/>
-
-                <label for="Parcelamento"></label>
-
-                <select name="Parcelamento" id=" Parcelamento ">
-                    <option value="Escolha uma forma de Parcelamento">Escolha uma forma de Parcelamento</option>
-                    <option value="5X Sem juros">5X Sem juros</option>
-                    <option value="10X Sem juros">10X Sem juros</option>
-                    <option value="15X Sem juros">15X Sem juros</option>
-                    <option value="20X Sem juros">20X Sem juros</option>
-
-                </select>
-
-
+                    
+                <div >
             
-            </div>
+                    <label for="Banco"></label>
 
-            <br/>
-            <br/>
-            <button className="button3"> Solicitar Análise </button>
+                    <select name="Banco" id=" Banco ">
+
+                        <option value="Escolha um Banco" >Escolha um Banco</option>
+                        <option value="Brasil">Brasil</option>
+                        <option value="Bradesco">Bradesco</option>
+                        <option value="Santander">Santander</option>
+                        <option value="Itaú">Itaú</option>
+
+                    </select>
+
+                    <br/>
+                    <br/>
+
+                    <label for="Parcelamento"></label>
+
+                    <select name="Parcelamento" id=" Parcelamento ">
+                        <option value="Escolha uma forma de Parcelamento">Escolha uma forma de Parcelamento</option>
+                        <option value="5X Sem juros">5X Sem juros</option>
+                        <option value="10X Sem juros">10X Sem juros</option>
+                        <option value="15X Sem juros">15X Sem juros</option>
+                        <option value="20X Sem juros">20X Sem juros</option>
+
+                    </select>
+
+
+                
+                </div>
+
+                <br/>
+                <br/>
+                <button className="button3"> Solicitar Análise </button>
+
+            </div>
 
         </div>
+
    </div>
 
 
