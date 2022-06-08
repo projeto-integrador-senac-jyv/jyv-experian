555f77018fad2a3b8fe520cc7ce04d88994bc642
julia-stefany <105455990+julia-stefany@users.noreply.github.com>
2022-06-08 14:58:34 -0300

    tela analise cadastro

--- src/AnaliseSimulacao.js
+++ src/AnaliseSimulacao.js
@@ -17,7 +17,7 @@ const AnaliseSimulacao = () => {
                 
                 <h1 > Parabéns! Seu cadastro foi efetuado com sucesso. </h1>
                 <img  width={500} src="https://i.imgur.com/Qz3uAWl.png"/>  
-                <h1> Aguarde, pois suas informações está em análise...  </h1>
+                <h1> Aguarde, pois suas informações estão em análise...  </h1>
                 
                 
             </div>
