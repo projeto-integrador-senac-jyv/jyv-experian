0c17f94f3fbb2dcda058bc61a94a1571d223b9f4
Júlia Jóia <105455881+julia-joiab@users.noreply.github.com>
2022-06-09 17:02:59 -0300

    alterações

--- src/AdmCadastro.js
+++ src/AdmCadastro.js
@@ -12,12 +12,25 @@ const AdmCadastro = () => {
 
             <Menu/>
 
-            <br/>
-            <br/>
-            <h1> Novas Solicitações: </h1>
-            <br/>
-            <br/>
+            <div>
 
+                <br/>
+                <br/>
+                <h1> Novas Solicitações: </h1>
+                <br/>
+                <br/>
+
+            </div>
+
+            <div className="Caixa2" >
+
+                <h4> Nome completo: </h4>
+                <h4> Data de nascimento: </h4>
+                <h4> CPF: </h4>
+                <h4> E-Mail: </h4>
+
+            </div>
+            
             <Rodape/>
 
         </div>
