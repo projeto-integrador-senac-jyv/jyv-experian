dc117138b1ea5d3804cb598be9571c291cb5a07a
joao-rios-siqueira <105453781+joao-rios-siqueira@users.noreply.github.com>
2022-06-13 14:35:45 -0300

    Update

--- src/MiniMenu.js
+++ src/MiniMenu.js
@@ -14,7 +14,7 @@ const MiniMenu = () => {
             <div className='container_mini_menu' >
 
                 <FontAwesomeIcon icon={faUserAstronaut} />
-                <Link to=''  > Meu perfil </Link>
+                <Link to='/perfil'  > Meu perfil </Link>
                 <br/>
                 <FontAwesomeIcon icon={faHandshake} />
                 <Link to='/parcerias' > Parceiros </Link>
