3c50b353858505a81d9852b50deaa757b8a2d4fe
julia-stefany <105455990+julia-stefany@users.noreply.github.com>
2022-06-15 16:50:43 -0300

    troca de lugar

--- src/Parcerias.js
+++ src/Parcerias.js
@@ -23,25 +23,26 @@ function Parceirias() {
                 <br/>
                 
                 <img  src='https://www.jornalcontabil.com.br/wp-content/uploads/2018/11/concurso-pu%CC%81blico-bb-cursodememorizac%CC%A7a%CC%83o-2.jpg'/>
-                <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdz4TKXFHeFkN2c3MEfnk-Y6luWVfH-be5PMcorG67i7wk2rEEzA73KQBY-yD83J89Xmc&usqp=CAU'/>
-                <img  src='https://banco.bradesco/assets/classic/img/linkedin.png'/>
+                <img  src='https://escolaeducacao.com.br/wp-content/uploads/2019/06/caixa-banco-codigo-104-750x430.jpeg'/>
+                <img  src='https://newcdn.guiadoinvestidor.com.br/gdi-static/2020/10/itau-logo.jpg'/>
                 <img src='https://media.tribunadointerior.com.br/2022/03/5e612fcd-santander.png'/>
                 <br/>
                 <br/>
                 <br/>
-                
-                <img  src='https://escolaeducacao.com.br/wp-content/uploads/2019/06/caixa-banco-codigo-104-750x430.jpeg'/>
-                <img src='https://hypermais.com/wp-content/uploads/2022/01/emprestimo-sim-como-contratar-6102.jpg'/>
-                <img  src='https://newcdn.guiadoinvestidor.com.br/gdi-static/2020/10/itau-logo.jpg'/>
-                <img  src='https://www.oficinadanet.com.br/imagens/post/32145/picpay.png'/>
-
-                <br/>
-                <br/>
-                <br/>
-                <img  src='https://www.datocms-assets.com/39397/1614171395-open-graph-logo-large-br.png'/>
+               
+                <img  src='https://banco.bradesco/assets/classic/img/linkedin.png'/>
                 <img src="https://emprestimofacil.com/blog/wp-content/uploads/2018/08/banco-pan.jpg"/>
-                <img  src='https://tecnonoticias.com.br/blogs/humor-do-mercado/wp-content/uploads/2021/11/Banco-Inter-via-Youtube-1024x556.png'/>
+                <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdz4TKXFHeFkN2c3MEfnk-Y6luWVfH-be5PMcorG67i7wk2rEEzA73KQBY-yD83J89Xmc&usqp=CAU'/>
                 <img  src='https://www.foregon.com/blog/wp-content/uploads/2020/05/telefonesparacontatobanrisul.jpeg'/>
+                <br/>
+                <br/>
+                <br/>
+                
+                <img  src='https://tecnonoticias.com.br/blogs/humor-do-mercado/wp-content/uploads/2021/11/Banco-Inter-via-Youtube-1024x556.png'/>
+                <img  src='https://www.datocms-assets.com/39397/1614171395-open-graph-logo-large-br.png'/>
+                
+                <img  src='https://www.oficinadanet.com.br/imagens/post/32145/picpay.png'/>
+                <img src='https://hypermais.com/wp-content/uploads/2022/01/emprestimo-sim-como-contratar-6102.jpg'/>
                 <br/>
                 <br/>
                 <br/>
