0e559d00b2fe07e9227203fa98db0b58b584c87b
Júlia Jóia <105455881+julia-joiab@users.noreply.github.com>
2022-06-30 13:29:52 -0300

    a

--- src/Comofunciona.js
+++ src/Comofunciona.js
@@ -15,10 +15,27 @@ const ComoFunciona = () => {
 
            
             <h1 className="roxo"> Como funciona o empréstimo online da JYV? </h1>
+            <br/>
+            <br/>
 
             <div className="fundoAdm" >
 
-                <div className="boxAdm" >
+                <img className='animate__animated animate__fadeIn' src='https://i.imgur.com/d4BQbQI.png' />
+                <img className='animate__animated animate__fadeIn' src='https://i.imgur.com/lN4sicZ.png' />
+
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+
+                <Link to='/Simulacao' className="button4" > Simule agora! </Link>
+
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+
+                {/* <div className="boxAdm" >
 
                     <p> Em poucos cliques, você pode ter em sua conta bancária o seu empréstimo pessoal online da JYV.  </p>
                     <h4> E-mails de aprovação recebidos em até 24h </h4>
@@ -28,7 +45,7 @@ const ComoFunciona = () => {
                     <br/>
                     <br/>
                     
-                </div>
+                </div> */}
 
 
             </div>
--- src/FaleConosco.js
+++ src/FaleConosco.js
@@ -40,7 +40,7 @@ function FaleConosco() {
                         <p> Endereço: Av. Pres. Juscelino Kubitscheck, 100 </p>
                     </div>
                     
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3698.2386925713113!2d-47.87811808443349!3d-22.04046301307359!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94b876566ad4700b%3A0xe030b5b189e95c7e!2sSerasa%20Experian!5e0!3m2!1spt-BR!2sbr!4v1654883114533!5m2!1spt-BR!2sbr" width="100%" height="500" style={{border:0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    <iframe className='animate__animated animate__fadeIn' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3698.2386925713113!2d-47.87811808443349!3d-22.04046301307359!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94b876566ad4700b%3A0xe030b5b189e95c7e!2sSerasa%20Experian!5e0!3m2!1spt-BR!2sbr!4v1654883114533!5m2!1spt-BR!2sbr" width="100%" height="500" style={{border:0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 
                 </div>
 
