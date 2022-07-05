9283d55f00292902992e6b982ca541030b9bb5f6
Júlia Jóia <105455881+julia-joiab@users.noreply.github.com>
2022-07-05 18:16:52 -0300

    projeto final

--- src/AdmEmprestimos.js
+++ src/AdmEmprestimos.js
@@ -2,8 +2,8 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import Menu from "./Menu";
 import Rodape from "./Rodape";
-import {Swiper, SwiperSlide} from 'swiper/react';
-import 'swiper/css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft, faCaretLeft, faCaretRight, faLeftLong, faRightLong } from "@fortawesome/free-solid-svg-icons";
 
 const AdmEmprestimos = () => {
     
@@ -12,6 +12,9 @@ const AdmEmprestimos = () => {
     const navigate = useNavigate()
     
     const [emprestimos, alteraemprestimos]=React.useState([])
+    const [ indice, alteraIndice ] = React.useState(0);
+
+    const mostrando = emprestimos[indice];
 
     React.useEffect(()=>{
 
@@ -58,7 +61,24 @@ const AdmEmprestimos = () => {
 
     const axios = require('axios').default;
     
+    const formataData = ( data ) => {
+        //formatar data de nascimento
+        let nascimento = data
+        nascimento = nascimento.split("T")
+        nascimento = nascimento[0]
+        nascimento = nascimento.split("-")
+        nascimento.reverse();
+        nascimento = nascimento.join("/")
+        return nascimento;
+    }
 
+    const formataHora = ( hora ) => {
+        // //formatar horario
+        hora = hora.split("T")[1]
+        hora = hora.split(".")[0]
+        hora = hora.split(":")[0] + ":" + hora.split(":")[1]
+        return hora;
+    }
 
 
     React.useEffect(()=>{
@@ -82,76 +102,58 @@ const AdmEmprestimos = () => {
                 
             </div>
 
-            {emprestimos==0? <p>  Carregando... </p>:
+            {
+                emprestimos==0? <p>  Carregando... </p>:
           
             
-            <>
-                {emprestimos.map(e => {
+            <div className="Carrossel">
 
+                <div onClick={()=>{ indice <= 0 ? alteraIndice(emprestimos.length-1) : alteraIndice(indice-1) }} >
+                    <FontAwesomeIcon icon={faCaretLeft} className='seta'/>
+                </div>
 
-                    //formatar data de nascimento
-                    let nascimento = e.nascimento
-                    nascimento = nascimento.split("T")
-                    nascimento = nascimento[0]
-                    nascimento = nascimento.split("-")
-                    nascimento.reverse();
-                    nascimento = nascimento.join("/")
+                {
+                    mostrando == null ? <p> Aguardando... </p> :
 
-                    let pedido_data = e.data_pedido
-                    pedido_data = pedido_data.split("T")
-                    pedido_data = pedido_data[0]
-                    pedido_data = pedido_data.split("-")
-                    pedido_data.reverse();
-                    pedido_data = pedido_data.join("/")
-                
-                    // //formatar horario
-                    let hora = e.data_pedido
-                    hora = hora.split("T")[1]
-                    hora = hora.split(".")[0]
-                    hora = hora.split(":")[0] + ":" + hora.split(":")[1]
-
-                    return(
-
-                        
 
                         <div className="Caixas" >
 
-                            <div className="Colunas">
+                            <div className="Colunas  ">
 
                                 <h4> Forma de parcelamento: </h4>
-                                <h2>{e.parcelamento}</h2>
+                                <h2>{mostrando.parcelamento}</h2>
                                 <br/>
 
                                 <h4> Data de nascimento: </h4>
-                                <h2>{nascimento}</h2>
+                                <h2>{ formataData(mostrando.nascimento)}</h2>
                                 <br/>
 
                                 <h4> CPF ou CNPJ: </h4>
-                                <h2>{e.cpf_cnpj}</h2>
+                                <h2>{mostrando.cpf_cnpj}</h2>
                                 <br/>
 
                                 <h4> E-Mail: </h4>
-                                <h2>{e.email}</h2>                                    
+                                <h2>{mostrando.email}</h2>                                    
                                 <br/>
 
                                 <h4> Valor: </h4>
-                                <h2>{e.valor}</h2>
+                                <h2>{mostrando.valor}</h2>
                                 <br/>
 
                                 <h4> Banco: </h4>
-                                <h2>{e.banco}</h2>
+                                <h2>{mostrando.banco}</h2>
                                 <br/>
 
-                                <h4> Data do envio: </h4>
+                                <h4> Data do envio: { formataData(mostrando.data_pedido)+" às "+formataHora(mostrando.data_pedido) } </h4>
 
                                 <div>
-                                    {e.status==0? <p className="recusado"> Status: Recusado </p>:e.status==1? <p className="aprovado"> Status: Aprovado </p>:<p className="aguardando"> Status: Aguardando</p>}
+                                    {mostrando.status==0? <p className="recusado"> Status: Recusado </p>:mostrando.status==1? <p className="aprovado"> Status: Aprovado </p>:<p className="aguardando"> Status: Aguardando</p>}
                                 </div>
 
                                 <div className="ButtonAdmEmprestimos">
 
-                                    <button className="AprovarAdmEmprestimos"  onClick={()=> muda_status(e.id_emprestimos,1)}> Aprovar </button>
-                                    <button className="RecusarAdmEmprestimos" onClick={()=> muda_status(e.id_emprestimos,0)} > Recusar </button>
+                                    <button className="AprovarAdmEmprestimos"  onClick={()=> muda_status(mostrando.id_emprestimos,1)}> Aprovar </button>
+                                    <button className="RecusarAdmEmprestimos" onClick={()=> muda_status(mostrando.id_emprestimos,0)} > Recusar </button>
 
                                 </div>
 
@@ -159,19 +161,25 @@ const AdmEmprestimos = () => {
 
                         </div>
 
-                    )
+                    
 
-                })}
+                }
 
-                <Rodape/>
+                <div onClick={()=>{ indice >= emprestimos.length-1 ? alteraIndice(0) : alteraIndice(indice+1) }} >
+                    <FontAwesomeIcon icon={faCaretRight} className='seta' />
+                </div>
 
-            </>      
-}
+                </div>
+
+
+            }
+
+            
+            <Rodape/>
+
         </div>
 
     );
 }
  
 export default AdmEmprestimos;
-
-// {e.status==0? <p className="recusado"> Status: Recusado </p>:e.status==1? <p className="aprovado"> Status: Aprovado </p>:<p className="aguardando"> Status: Aguardando</p>}
